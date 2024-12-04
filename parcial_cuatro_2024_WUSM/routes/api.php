cb8d7142bd8314e018d261b47583a42b53c178bf
Wilman Uriel Salmeron Martinez <mrwilman053@gmail.com>
2024-12-03 19:57:34 -0600

    parcial cuatro

---
+++ parcial_cuatro_2024_WUSM/routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::post('/nuevousuario', [AuthController::class,"register"]);
+Route::post('/Login', [AuthController::class,"login"]);
